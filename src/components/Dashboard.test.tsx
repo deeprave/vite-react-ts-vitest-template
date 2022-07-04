5fccdbb17aca0d58203ae50ca8eafa3656069d16
David Nugent <davidn@uniquode.io>
2022-07-04 22:36:42 +1000

    Add missing test for About component
    - Also explitly import items from vitest as it does not make symbols global (and webstorm complains)

--- src/components/Dashboard.test.tsx
+++ src/components/Dashboard.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import {render, screen} from "@testing-library/react";
+import {describe, expect, it} from 'vitest'
+import {render, screen} from "@testing-library/react"
 import {Dashboard} from './Dashboard'
 
 
